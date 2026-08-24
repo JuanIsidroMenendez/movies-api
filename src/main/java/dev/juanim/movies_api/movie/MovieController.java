05cfa94d77aa3f08ab8caf504062107211853280
Juan <juan.isidro.menendez@gmail.com>
2026-08-24 12:54:44 +0200

    feat: update MovieController with post, put and delete endpoints

--- src/main/java/dev/juanim/movies_api/movie/MovieController.java
+++ src/main/java/dev/juanim/movies_api/movie/MovieController.java
@@ -1,32 +1,59 @@
 package dev.juanim.movies_api.movie;
 
 import dev.juanim.movies_api.implementations.InterfaceGenericGetService;
+import dev.juanim.movies_api.implementations.InterfaceGenericWriteService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+/* Imports relativos a interfaz Write */
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
 
-    private final InterfaceGenericGetService<Movie> movieService;
+    private final InterfaceGenericGetService<Movie> movieGetService;
+    private final InterfaceGenericWriteService<Movie> movieWriteService;
+  
 
-    public MovieController(InterfaceGenericGetService<Movie> movieService) {
-        this.movieService = movieService;
+    public MovieController(
+        InterfaceGenericGetService<Movie> movieGetService,
+        InterfaceGenericWriteService<Movie> movieWriteService)
+        {
+        this.movieGetService = movieGetService;
+        this.movieWriteService = movieWriteService;
     }
 
     @GetMapping
     public List<Movie> getAllMovies() {
-        return movieService.getEntities();
+        return movieGetService.getEntities();
     }
 
     @GetMapping("/id")
     public Movie getMovieById(@PathVariable Long id) {
-        return movieService.getById(id);
+        return movieGetService.getById(id);
     }
-    
+
+    @PostMapping /* @Request convierte el JSON entrante en Movie */
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieWriteService.create(movie);
+    }
+    @PutMapping("/{id}")
+    public Movie updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+        return movieWriteService.update(id, movie); 
+    }
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable Long id) {
+        movieWriteService.delete(id);
+    }
 }
