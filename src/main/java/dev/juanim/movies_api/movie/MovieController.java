70e28ba2ea049fe166a971e15fb9e23778d0bd4e
Juan <juan.isidro.menendez@gmail.com>
2026-08-24 12:01:41 +0200

    feat: update MovieController with getById endpoint

--- src/main/java/dev/juanim/movies_api/movie/MovieController.java
+++ src/main/java/dev/juanim/movies_api/movie/MovieController.java
@@ -1,26 +1,34 @@
-package dev.juanim.movies_api.controllers;
+package dev.juanim.movies_api.movie;
 
-import dev.juanim.movies_api.movie.Movie;
-import dev.juanim.movies_api.movie.MovieRepository;
+import dev.juanim.movies_api.implementations.InterfaceGenericGetService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
 
-    private final MovieRepository movieRepository;
+    private final InterfaceGenericGetService<Movie> movieService;
 
-    public MovieController(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
+    public MovieController(InterfaceGenericGetService<Movie> movieService) {
+        this.movieService = movieService;
     }
 
     @GetMapping
     public List<Movie> getAllMovies() {
-        return movieRepository.findAll();
+        return movieService.getEntities();
     }
+
+    @GetMapping("/id")
+    public Movie getMovieById(@PathVariable Long id) {
+        return movieService.getById(id);
+    }
+    
 }
