5b8f39b2c5118f3665845363eb468973ee503638
Juan <juan.isidro.menendez@gmail.com>
2026-08-26 11:49:14 +0200

    feat: add findByTitleContainingIgnoreCase query to repository

--- src/main/java/dev/juanim/movies_api/movie/MovieRepository.java
+++ src/main/java/dev/juanim/movies_api/movie/MovieRepository.java
@@ -2,5 +2,10 @@ package dev.juanim.movies_api.movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByTitleContainingIgnoreCase(String title);
+
 }
